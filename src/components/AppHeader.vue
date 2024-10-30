d179410bd06097ff156b6e2ee925f511464aa54b
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-10-30 13:43:12 +0100

    added function d-none and d-block for visibility city

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,7 +8,7 @@ export default {
     data() {
         return {
             store,
-            city: [],
+
         }
     },
 
@@ -19,22 +19,33 @@ export default {
                     .get(store.baseUrlApiTomTom + store.query + '.json', { params: store.params })
                     .then(response => {
 
-                        this.city = response.data.results;
+                        this.store.city = response.data.results;
 
                     })
                     .catch(error => {
                         console.error('Errore nella richiesta:', error);
                     });
             }
-
         },
 
-        selectedCity(name, latitude, longitude) {
+        dNone() {
+            document.getElementById('city').classList.add('d-none');
+        },
+
+        dBlock() {
+            document.getElementById('city').classList.remove('d-none');
+        },
+
+        selectedCity(name, latitude, longitude, countrySubdivision, countryCode) {
             store.queryMeteo = {
                 cityName: name,
                 cityLatitude: latitude,
                 cityLongitude: longitude,
+                cityCountrySubdivision: countrySubdivision,
+                cityCountryCode: countryCode
             }
+            this.dNone();
+            store.query = store.queryMeteo.cityName + ', ' + store.queryMeteo.cityCountrySubdivision + ', ' + store.queryMeteo.cityCountryCode;
             console.log(store.queryMeteo)
         },
 
@@ -44,34 +55,40 @@ export default {
                 .then(response => {
                     store.queryResult = response.data
                     console.log(store.queryResult)
-                })
-        }
+                });
+
+            store.currentCityName = store.queryMeteo.cityName;
+            store.query = ""
+        },
+
     },
 }
 </script>
 
 <template>
     <header class="bg-primary">
-        <div class="container">
-            <div class="py-2 row m-0 justify-content-between align-items-center">
-                <h2 class="m-0 py-2 col-auto">Cerca il Meteo</h2>
-                <div class="m-0 py-2 col-auto position-relative">
-                    <input type="text" list="city" name="city" id="" placeholder="Nome della Città"
-                        @input="searchCityMeteo()" v-model="store.query">
-                    <div class="bg-white position-absolute top-25 left-25" id="city">
-                        <template v-for="singleCity in city">
-                            <p
-                                @click="selectedCity(singleCity.address.municipality, singleCity.position.lat, singleCity.position.lon)">
-                                {{ singleCity.address.municipality + ' ' + singleCity.address.countrySubdivision + ' ' +
-                                    singleCity.address.countryCode }}
-                            </p>
-                        </template>
-                    </div>
-                    <button type="submit" class="px-2 py-2 ms-2 rounded-2" @click="resultCity()">Cerca</button>
+        <div class="py-2 row m-0 justify-content-between align-items-center">
+            <h2 class="m-0 py-2 col-auto">Cerca il Meteo della tua Città</h2>
+            <div class="m-0 py-2 col-auto position-relative">
+                <input type="text" list="city" name="city" id="" placeholder="Nome della Città" @click="dBlock()"
+                    @input="searchCityMeteo()" v-model="store.query">
+                <div class="bg-white position-absolute top-25" id="city">
+                    <template v-for="singleCity in store.city">
+                        <p
+                            @click="selectedCity(singleCity.address.municipality, singleCity.position.lat, singleCity.position.lon, singleCity.address.countrySubdivision, singleCity.address.countryCode)">
+                            {{ singleCity.address.municipality + ', ' + singleCity.address.countrySubdivision + ', '
+                                + singleCity.address.countryCode }}
+                        </p>
+                    </template>
                 </div>
+                <button type="submit" class="px-2 py-2 ms-2 rounded-2" @click="resultCity()">Cerca</button>
             </div>
         </div>
     </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+p {
+    cursor: pointer
+}
+</style>
